62e9000e4850fd39146069e23da4d3d99de971eb
ELIJAH <vitoesitemitope@gmail.com>
2023-04-13 18:04:56 +0100

    Update some functions to arrow functions

--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -13,22 +13,22 @@ export default class Leaderboard {
   }
 
    display = async () => {
-    const scoresList = document.querySelector('#scores__list');
-    scoresList.innerHTML = '';
-    try {
-      this.leaders = await Api.get();
-      // Sort the result desc
-      this.leaders = this.leaders.sort((a, b) => b.score - a.score);
-      this.leaders.forEach((leader) => {
-        // New li
-        const li = document.createElement('li');
-        li.textContent = `${leader.user}:  ${leader.score}`;
-        scoresList.appendChild(li);
-      });
-    } catch (error) {
-      const li = document.createElement('li');
-      li.textContent = 'Please, verify your network connection';
-      scoresList.appendChild(li);
-    }
-  }
+     const scoresList = document.querySelector('#scores__list');
+     scoresList.innerHTML = '';
+     try {
+       this.leaders = await Api.get();
+       // Sort the result desc
+       this.leaders = this.leaders.sort((a, b) => b.score - a.score);
+       this.leaders.forEach((leader) => {
+         // New li
+         const li = document.createElement('li');
+         li.textContent = `${leader.user}:  ${leader.score}`;
+         scoresList.appendChild(li);
+       });
+     } catch (error) {
+       const li = document.createElement('li');
+       li.textContent = 'Please, verify your network connection';
+       scoresList.appendChild(li);
+     }
+   }
 }
